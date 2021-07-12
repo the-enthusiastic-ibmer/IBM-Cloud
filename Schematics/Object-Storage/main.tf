6755cbba01cf7492abee4e9f310393beebacb9d4
the-enthusiastic-ibmer <86448545+the-enthusiastic-ibmer@users.noreply.github.com>
2021-07-11 21:04:51 -0500

    Update main.tf

--- Schematics/Object-Storage/main.tf
+++ Schematics/Object-Storage/main.tf
@@ -7,15 +7,24 @@
 data "ibm_resource_group" "resgroup" {
   is_default = "true"
 }
-data "ibm_resource_instance" "my_instance" {
+
+resource "ibm_resource_instance" "resinstance" {
   name = var.cos_instance_name-1
+  service = "cloud-object-storage"
+  plan = "lite"
+  location = "global"
   resource_group_id = data.ibm_resource_group.resgroup.id
+}
+
+data "ibm_resource_instance" "resinstance_id" {
+  name = var.cos_instance_name-1
   service = "cloud-object-storage"
   location = "global"
 }
+
 resource "ibm_cos_bucket" "my_bucket" {
   bucket_name = var.bucket_name-1
-  resource_instance_id = data.ibm_resource_instance.my_instance.id
+  resource_instance_id = data.ibm_resource_instance.resinstance_id.id
   region_location = var.region
   storage_class = "standard"
 }
