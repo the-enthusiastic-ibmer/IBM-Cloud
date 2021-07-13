1181451c277ffac65286e0664e32da7a05f73e6e
the-enthusiastic-ibmer <86448545+the-enthusiastic-ibmer@users.noreply.github.com>
2021-07-13 00:46:16 -0500

    Update versions.tf
    Updated provider source location (source = IBM-Cloud/ibm)

--- Schematics/openshift-cluster/versions.tf
+++ Schematics/openshift-cluster/versions.tf
@@ -1,3 +1,9 @@
 terraform {
-  required_version = ">= 0.12"
+   required_version = ">= 0.12"
+   required_providers {
+      ibm = {
+         source = "IBM-Cloud/ibm"
+         version = "1.27.2"
+      }
+    }
 }
